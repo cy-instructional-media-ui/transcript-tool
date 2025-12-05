cee2494c27b2bdbbe656b472a8a3c70097d78a4d
cy-instructional-media-ui <cyrushelf@gmail.com>
2025-12-05 15:06:48 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',            // <-- ensures Vite uses index.html at project root
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
